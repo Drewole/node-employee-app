5f3fa47747a317c8d7c1ae2834423b59d958e89b
Drew Olsen <drew@drewolsendesign.com>
2021-04-06 23:13:37 -0500

    Added testing files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {managerQuestions,
       internQuestions,
       addEmployeeConfirm} = require("./questions")
 const Employee = require("./lib/Employee")
-// const Manager = require("./lib/Manager")
+const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 
@@ -13,14 +13,12 @@ var questionGathering = function(managerQuestions,engineerQuestions,internQuesti
   return Inquirer
     .prompt(managerQuestions)
     .then(function(answers){
-      console.log(answers)
-        // if (answers.officeNum === true){
-        //   //loop back to third prompt again
-          
-        // }else{
-        //   //ask 1st prompt
-        //   return questionGathering('questions 1');
-        // }
+      // console.log(answers)
+      
+
+
+        if (answers.officeNum === true){
+        }
     })
   
 }
